673ba8343cbc63339d0a66ae4bdbd88af7f7150d
0023412555LeTuanLinh <0023412555@student.dthu.edu.vn>
2025-11-23 09:38:08 +0700

    Delete MyBlazorApp directory

--- MyBlazorApp/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyBlazorApp.Models
-{
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty; // tránh lỗi non-nullable
-        public decimal Price { get; set; } = 0m;
-    }
-}
